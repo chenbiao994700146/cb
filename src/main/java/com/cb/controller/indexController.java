1772bc7dd4084d1c22976ecb2ed57cd4dd750c8e
chenbiao <994700146@qq.com>
2022-05-18 13:44:34 +0800

    one

---
+++ src/main/java/com/cb/controller/indexController.java
@@ -0,0 +1,11 @@
+package com.cb.controller;
+
+/**
+ * @author chenbiao
+ * @Data 2022/5/18 11:31 AM
+ */
+public class indexController {
+    public static void main(String[] args) {
+        System.out.println("h");
+    }
+}
